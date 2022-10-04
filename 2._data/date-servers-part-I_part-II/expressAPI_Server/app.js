cd0d9db9840d473260d49e241376b4b0c68a88ab
Cris <mccrys_00@yahoo.com>
2022-10-04 15:31:38 +0200

    Cleanup Ex 2

--- 2._data/date-servers-part-I_part-II/expressAPI_Server/app.js
+++ 2._data/date-servers-part-I_part-II/expressAPI_Server/app.js
@@ -3,16 +3,16 @@ const app = express()
 const port = 3000
 const API_URL = "http://127.0.0.1:8000"
 
+// GET request which provides current timestamp (exercise part I)
 app.get("/express_api_timestamp", (req, res) => {
   res.send({"timestamp": new Date()}) 
 })
 
+// Integrating third-party API (exercise part II)
 app.get("/timestamp", async (req, res) => {
   let result = await fetch(`${API_URL}/fast_api_timestamp`)
   result = await result.json()
-  // console.log(typeof result.timestamp)
   const date = new Date(result.timestamp)
-  // console.log(typeof date)
   res.send({"Timestamp from FAST_API": date})
 })
 
--- 2._data/date-servers-part-I_part-II/fastAPI_Server/main.py
+++ 2._data/date-servers-part-I_part-II/fastAPI_Server/main.py
@@ -1,25 +1,25 @@
 # to run the server execute commands: 
+# $ poetry init
 # $ poetry install
+# $ poetry shell
 # $ uvicorn main:app --reload
 
 from fastapi import FastAPI
 from datetime import datetime
-
 import requests
 
 app = FastAPI()
 API_URL = "http://localhost:3000"
 
+#GET request which provides current timestamp (exercise part I)
+@app.get("/fast_api_timestamp")
+def _():
+    return {"timestamp": datetime.now()}
+
+# Integrating third-party API (exercise part II)
 @app.get("/timestamp")
 def _():
     response = requests.get(f'{API_URL}/express_api_timestamp')
     response = response.json()
-    
     current_date = datetime.strptime(response['timestamp'], '%Y-%m-%dT%H:%M:%S.%fZ')
-    print(type(current_date))
-
     return {"Timestamp from EXPRESS_API": current_date} 
-
-@app.get("/fast_api_timestamp")
-def _():
-    return {"timestamp": datetime.now()}
